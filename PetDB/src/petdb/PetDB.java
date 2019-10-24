9078fac0ef44fc86f0c10085d96fcbf65ae1aa70
adammaser <adammaser@localhost>
2019-10-24 11:49:58 -0500

    Added functionality to update and remove pets. Also organized code and added comments.

--- PetDB/src/petdb/PetDB.java
+++ PetDB/src/petdb/PetDB.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * CSC 422
+ * Adam Maser
+ * 10/27/2019
+ * assignment 1: version control practice with pet program
  */
 package petdb;
 
@@ -13,23 +14,23 @@ import java.util.Scanner;
  */
 public class PetDB {
     static ArrayList<Pet> allPets = new ArrayList<>();
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         Scanner stdin = new Scanner(System.in);
-        boolean userChoice = true;
         System.out.println("Pet Database Program.");
-        while (userChoice) {
+        while (true) {
             System.out.print("\nWhat would you like to do?\n"
                 + " 1) View all pets\n"
                 + " 2) Add more pets\n"
-                + " 3) Search by name\n"
-                + " 4) Search by age\n"
-                + " 5) Exit program\n"
+                + " 3) Update an existing pet\n"
+                + " 4) Remove an existing pet\n"
+                + " 5) Search by name\n"
+                + " 6) Search by age\n"
+                + " 7) Exit program\n"
                 + "Your choice: ");
             int selection = stdin.nextInt();
             
+            //program choice control
             switch(selection) {
                 case 1: 
                     displayPets();
@@ -38,80 +39,127 @@ public class PetDB {
                     addPets();
                     break;
                 case 3:
-                    searchByName();
+                    updatePet();
                     break;
                 case 4:
-                    searchByAge();
+                    removePet();
                     break;
                 case 5:
+                    searchByName();
+                    break;
+                case 6:
+                    searchByAge();
+                    break;
+                case 7:
                     System.exit(0);
             }
         }
     }
+    /*
     
+        Methods to handle each choice
+    
+    */
     private static void displayPets() {
+        //print header
         System.out.print("\n+----------------------+\n");
         System.out.printf("|%3s | %-10s|%4s |", "ID", "NAME", "AGE");
         System.out.print("\n+----------------------+\n");
+        //loop through all pets and add to table
         for (int i = 0; i< allPets.size(); i++) {
             Pet pet = allPets.get(i);
             System.out.printf("|%3d | %-10s|%4d |\n", i, pet.getName(), pet.getAge());
         }
-        System.out.println("+----------------------+");
+        System.out.println("+----------------------+");//print footer
     }
     
     private static void addPets() {
-        int petsAdded = 0;
+        int petsAdded = 0; //stores amount of pets added
         System.out.println();
         while (true) {
             Scanner stdin = new Scanner(System.in);
             System.out.print("add pet (name, age): ");
             String response = stdin.nextLine();
+            //if user types 'done', addPets exits
             if (response.equals("done")) {
                 break;
             } else {
                 Scanner parseResponse = new Scanner(response);
+                
                 String petName = parseResponse.next();
                 int petAge = parseResponse.nextInt();
+                
                 Pet newPet = new Pet(petName, petAge);
                 allPets.add(newPet);
+                
                 petsAdded++;
             }
         }
         System.out.println(petsAdded + " pets added.");
     }
     
+    private static void updatePet() {
+        displayPets(); //displays all pets before prompting user
+        Scanner stdin = new Scanner(System.in);
+        System.out.print("Enter the pet ID you want to update: ");
+        int petToUpdate = stdin.nextInt();
+        //get pet
+        Pet pet = allPets.get(petToUpdate);
+        System.out.print("Enter new name and new age: ");
+        String newName = stdin.next();
+        int newAge = stdin.nextInt();
+        //update pet
+        pet.setName(newName);
+        pet.setAge(newAge);
+    }
+    
+    private static void removePet() {
+        displayPets(); //display all pets before prompting user
+        Scanner stdin = new Scanner(System.in);
+        System.out.print("Enter the pet ID you want to remove: ");
+        int petToDelete = stdin.nextInt();
+        //get pet
+        Pet pet = allPets.get(petToDelete);
+        System.out.println(pet.getName() + " " + pet.getAge() + " has been removed.");
+        //delete pet
+        allPets.remove(petToDelete);
+    }
+    
     private static void searchByName() {
+        //prompt user
         Scanner stdin = new Scanner(System.in);
         System.out.print("\nEnter a name to search: ");
         String nameToSearch = stdin.next();
-        
+        //print table header
         System.out.print("\n+----------------------+\n");
         System.out.printf("|%3s | %-10s|%4s |", "ID", "NAME", "AGE");
         System.out.print("\n+----------------------+\n");
+        //loop through each pet, adding to table if it matches name
         for (int i = 0; i< allPets.size(); i++) {
             Pet pet = allPets.get(i);
             if (pet.getName().equals(nameToSearch)) {
                 System.out.printf("|%3d | %-10s|%4d |\n", i, pet.getName(), pet.getAge());
             } 
         }
-        System.out.println("+----------------------+");
+        System.out.println("+----------------------+");//print footer
     }
     
     private static void searchByAge() {
+        //prompt user
         Scanner stdin = new Scanner(System.in);
         System.out.print("\nEnter an age to search: ");
         int ageToSearch = stdin.nextInt();
-        
+        //print header
         System.out.print("\n+----------------------+\n");
         System.out.printf("|%3s | %-10s|%4s |", "ID", "NAME", "AGE");
         System.out.print("\n+----------------------+\n");
+        //loop through each pet, adding to table if it matches age
         for (int i = 0; i< allPets.size(); i++) {
             Pet pet = allPets.get(i);
             if (pet.getAge() == ageToSearch) {
                 System.out.printf("|%3d | %-10s|%4d |\n", i, pet.getName(), pet.getAge());
             } 
         }
-        System.out.println("+----------------------+");
+        System.out.println("+----------------------+");//print footer
     }
 }
